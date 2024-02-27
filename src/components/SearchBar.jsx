dc4ebb64f57fd0f9075eb21ae4f5d578a2d8a166
Sameer Jha <sameerjha236@gmail.com>
2024-02-28 00:19:08 +0530

    Code fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAppContext } from "../context";
 const SearchBar = () => {
   const { curr, setCurr, submitHandler } = useAppContext();
