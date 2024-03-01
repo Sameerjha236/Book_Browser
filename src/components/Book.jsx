b97981fe7f6daff743114d4c94bb685638f4ca72
Sameer Jha <sameerjha236@gmail.com>
2024-03-01 07:34:13 +0530

    Book Structure updated

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,38 +1,29 @@
 import React, { useState } from "react";
 
 const Book = ({ book }) => {
-  const { title, authors, imageLinks, description } = book;
-  const [showFullDescription, setShowFullDescription] = useState(false);
-
-  const toggleDescription = () => {
-    setShowFullDescription(!showFullDescription);
-  };
-  // console.log("idhar ", book);
+  const [showDetail, setShowDetail] = useState(0);
+  const { title, authors, imageLinks, description, categories, date } = book;
   return (
-    <div className="flex w-full max-w-md mx-aut0  hover:bg-slate-300 shadow-md rounded-lg overflow-hidden">
-      <div className="w-1/2">
-        {imageLinks && imageLinks.smallThumbnail && (
-          <img
-            className="object-contain w-full h-auto"
-            src={imageLinks.smallThumbnail}
-            alt={title}
-          />
-        )}
+    <div className=" flex gap-2 w-2/3 justify-around items-center bg-slate-300 rounded-lg hover:bg-slate-400 transform hover:scale-105 transition-transform duration-500">
+      <div className="">
+        <img
+          className=" object-contain w-full h-auto"
+          src={imageLinks.smallThumbnail}
+          alt="{title}"
+        />
       </div>
-      <div className="w-1/2 p-4">
-        <h2 className="text-lg font-semibold mb-2">{title}</h2>
-        <p className="text-sm mb-2">Author: {authors.join(", ")}</p>
-        <p className="text-sm">
-          Description:{" "}
-          {showFullDescription
-            ? `${description.slice(0, 300)}...`
-            : `${description.slice(0, 100)}...`}
-        </p>
-        <button
-          className="text-sm text-blue-500 hover:underline focus:outline-none"
-          onClick={toggleDescription}
-        >
-          {showFullDescription ? "Read less" : "Read more"}
+      <div className="flex flex-col justify-center items-center gap-2 w-1/2">
+        <h1 className="font-bold text-2xl text-center">{title}</h1>
+        <h3 className="text-xl">{authors}</h3>
+        {showDetail && (
+          <div className="flex flex-col justify-center items-center">
+            <p>{description}</p>
+            <p>{categories}</p>
+            <p>{date}</p>
+          </div>
+        )}
+        <button onClick={() => setShowDetail(!showDetail)}>
+          {showDetail ? "Read Less" : "Read More..."}
         </button>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="Navbar w-full h-20 flex justify-center items-center bg-slate-300">
-      <h1>Book Browser</h1>
+      <h1 className="font-bold text-3xl">Book Browser</h1>
     </div>
   );
 };
