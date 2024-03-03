dd211813153607d57b2c7030454315c12111804c
Sameer Jha <sameerjha236@gmail.com>
2024-03-03 21:09:32 +0530

    Form created

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,14 +4,15 @@ const SearchBar = () => {
   const { curr, setCurr, submitHandler } = useAppContext();
   return (
     <div className="SearchBar flex w-52 sm:w-80">
-      <input
-        type="text"
-        value={curr}
-        onChange={(e) => setCurr(e.target.value)}
-        className="w-full rounded-full h-8 text-center text-black font-bold"
-        onKeyDown={submitHandler}
-        placeholder="Enter Book Name"
-      />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={curr}
+          onChange={(e) => setCurr(e.target.value)}
+          className="w-full rounded-full h-8 text-center text-black font-bold"
+          placeholder="Enter Book Name"
+        />
+      </form>
     </div>
   );
 };
